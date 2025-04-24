382c9ae879ec92f164dae0e73736368c63437ae9
Alexey-Efimov767 <alexefimov565@gmail.com>
2025-04-24 15:58:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -28,34 +28,53 @@ int countPairs2(int *arr, int len, int value) {
     }
     return count;
 }
+int countPairs(int *arr, int len, int value) {
+    int counter = 0;
+    for (int i = 0; i < len; ++i) {
+        for (int j = i + 1; j < len; ++j) {
+            if (arr[i] + arr[j] == value) {
+                counter++;
+            }
+        }
+    }
+    return counter;
+}
+
 int countPairs3(int *arr, int len, int value) {
-  int counter = 0;
-  for (int i = 0; i < len - 1; ++i) {
-    int targ = value - arr[i];
-    int left = i + 1, right = len - 1;
-    while (left <= right) {
-      int mid = (left + right) / 2;
-      if (arr[mid] < targ) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
-      }
+    int counter = 0;
+    for (int i = 0; i < len - 1; ++i) {
+        int targ = value - arr[i];
+        int left = i + 1, right = len - 1;
+
+        int low = len;
+        int high = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == targ) {
+                low = mid;
+                right = mid - 1;
+            } else if (arr[mid] < targ) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        left = i + 1;
+        right = len - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2; 
+            if (arr[mid] == targ) {
+                high = mid;
+                left = mid + 1;
+            } else if (arr[mid] < targ) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        if (low <= high && low < len && high >= 0) {
+            counter += (high - low + 1);
+        }
     }
-    int low = left;
-    left = i + 1;
-    right = len - 1;
-    while (left <= right) {
-      int mid = (left + right) / 2;
-      if (arr[mid] <= targ) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
-      }
-    }
-    int high = right;
-    if (low <= high) {
-      counter += (high - low + 1);
-    }
-  }
-  return counter;
+    return counter;
 }
