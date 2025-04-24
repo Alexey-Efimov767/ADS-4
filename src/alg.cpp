66babc728f6fa0362fc8cbd8b349c5912eb607b5
Alexey-Efimov767 <alexefimov565@gmail.com>
2025-04-24 15:47:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,60 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int counter = 0;
+  for (int i = *arr; i < len - 1; i++) {
+    for (int j = 1; j < len - 1; j++) {
+        if (arr[i] + arr[j] == value) {
+            counter++;
+        }
+    }
+  }
+  return counter;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+    int pos = len;
+    for (int i = 0; i < len; ++i) {
+        if (arr[i] > value) {
+            pos = i;
+            break;
+        }
+    }
+    for (int i = 0; i < pos; ++i) {
+        for (int j = i + 1; j < pos; ++j) {
+            if (arr[i] + arr[j] == value) {
+                count++;
+            }
+        }
+    }
+    return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+    for (int i = 0; i < len - 1; ++i) {
+        if (arr[i] > value)
+            break;
+
+        int target = value - arr[i];
+        int left = i + 1;
+        int right = len - 1;
+        int first = -1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == target) {
+                first = mid;
+                right = mid - 1;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        if (first != -1) {
+            int last = first;
+            while (last + 1 < len && arr[last + 1] == target)
+                ++last;
+            count += (last - first + 1);
+        }
+    }
+    return count;
 }
