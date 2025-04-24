46278339edea7673ca9ce2ee6badf855a3479b9e
Alexey-Efimov767 <alexefimov565@gmail.com>
2025-04-24 15:51:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -29,32 +29,33 @@ int countPairs2(int *arr, int len, int value) {
     return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  int count = 0;
-    for (int i = 0; i < len - 1; ++i) {
-        if (arr[i] > value)
-            break;
-
-        int target = value - arr[i];
-        int left = i + 1;
-        int right = len - 1;
-        int first = -1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (arr[mid] == target) {
-                first = mid;
-                right = mid - 1;
-            } else if (arr[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        if (first != -1) {
-            int last = first;
-            while (last + 1 < len && arr[last + 1] == target)
-                ++last;
-            count += (last - first + 1);
-        }
+  int counter = 0;
+  for (int i = 0; i < len - 1; ++i) {
+    int targ = value - arr[i];
+    int left = i + 1, right = len - 1;
+    while (left <= right) {
+      int mid = (left + right) / 2;
+      if (arr[mid] < targ) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
     }
-    return count;
+    int low = left;
+    left = i + 1;
+    right = len - 1;
+    while (left <= right) {
+      int mid = (left + right) / 2;
+      if (arr[mid] <= targ) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+    int high = right;
+    if (low <= high) {
+      counter += (high - low + 1);
+    }
+  }
+  return counter;
 }
